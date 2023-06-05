d9d7233e511bac2f61bb3f432d9d6c33c975980c
PecanPie <hidra0321@naver.com>
2023-06-05 21:59:31 +0900

    SLL Example of getting mergin point - own Answer

--- CodeTest_SinglyLinkedList.playground/Pages/SLL2_example_MySolution.xcplaygroundpage/Contents.swift
+++ CodeTest_SinglyLinkedList.playground/Pages/SLL2_example_MySolution.xcplaygroundpage/Contents.swift
@@ -9,6 +9,17 @@ import Foundation
  
  */
 
+public func progressTime(_ closure: () -> ()) -> TimeInterval {
+    let start = CFAbsoluteTimeGetCurrent()
+    closure()
+    let diff = CFAbsoluteTimeGetCurrent() - start
+    print("--progress--")
+    print(diff)
+    print("----")
+
+    return (diff)
+}
+
 class Node {
     var data: Int
     var next: Node?
@@ -19,32 +30,48 @@ class Node {
     }
 }
 
+func length(_ head: Node?) -> Int {
+    if head == nil {
+        return 0
+    }
+    
+    var headNode: Node? = head
+    var length: Int = 0
+    
+    while headNode != nil {
+        headNode = headNode?.next
+        length += 1
+    }
+    
+    return length
+}
+
     // 내 solution: 들리게 되는 점에 count 를 올려서 처음 count 가 2가 되는 애를 리턴하자. 그렇지 않으면, nil 반환
 func findMerge(_ headA: Node?, _ headB: Node?) -> Int? {
-    var headCountDictionary: [Int: Int] = [:]
-    
+    var headCountDictionary: [Int: Bool] = [:]
+
     var nodeA: Node? = headA
     var nodeB: Node? = headB
-    
+
     while nodeA != nil {
-        if headCountDictionary[nodeA!.data] == 1 {
+        if headCountDictionary[nodeA!.data] == true {
             print("A")
             print(nodeA!.data)
             return nodeA!.data
         }
-        
-        headCountDictionary[nodeA!.data] = 1
+
+        headCountDictionary[nodeA!.data] = true
         nodeA = nodeA?.next
     }
-    
+
     while nodeB != nil {
-        if headCountDictionary[nodeB!.data] == 1 {
-            print("A")
+        if headCountDictionary[nodeB!.data] == true {
+            print("B")
             print(nodeB!.data)
             return nodeB!.data
         }
-        
-        headCountDictionary[nodeB!.data] == 1
+
+        headCountDictionary[nodeB!.data] == true
         nodeB = nodeB?.next
     }
     
@@ -66,16 +93,23 @@ func prointLinkedList(head: Node?) {
     print(result)
 }
 
-let node6 = Node(6)
+let node10 = Node(10)
+let node9 = Node(9, node10)
+let node8 = Node(8, node9)
+let node7 = Node(7, node8)
+let node6 = Node(6, node7)
 let node5 = Node(5, node6)
 let node4 = Node(4, node5)
 let node3 = Node(3, node4)
 let node2 = Node(2, node3)
 let node1 = Node(1, node2)
 
-let node11 = Node(11, node4)
-let node10 = Node(10, node11)
+let node21 = Node(21, node4)
+let node20 = Node(20, node21)
 
-findMerge(node1, node10)
+progressTime {
+    findMerge(node1, node20)
+}
 
+// Avg of 0.0021 ~ 0.0024 secs
 
