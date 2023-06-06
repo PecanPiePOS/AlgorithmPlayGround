287c9be296f1ba39099668dadf184d9bb41fc38d
PecanPie <hidra0321@naver.com>
2023-06-07 03:32:49 +0900

    Added Queue Example of LeetCode#387 - Wrong - Trying again tomorrow

--- CodeTest_Stacks_Queues.playground/Pages/Leet_387.xcplaygroundpage/Contents.swift
+++ CodeTest_Stacks_Queues.playground/Pages/Leet_387.xcplaygroundpage/Contents.swift
@@ -1,7 +1,34 @@
-//: [Previous](@previous)
 
 import Foundation
 
-var greeting = "Hello, playground"
+/*
+ 
+ Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+ 
+ */
+    // MARK: - Wrong Answer -Exception Case of s = "aaaddd" -> 5
+class Solution {
+    func firstUniqChar(_ s: String) -> Int {
+        var stringQueue = Array(s)
+        var characterDict: [Character: Int] = [:]
+                
+        for (index, character) in stringQueue.enumerated() { // N
+            if characterDict[character] == nil {
+                characterDict[character] = index
+            } else {
+                characterDict[character] = nil
+            }
+        }
+        
+        print(characterDict)
+        
+        if characterDict.isEmpty {
+            return -1
+        }
+        
+        guard let result = characterDict.sorted(by: { $0.value < $1.value }).first?.value else { return -1 }  // N logN
 
-//: [Next](@next)
+        return result
+    }
+}
+
