817fc8f514f420d83bf7d6267893369d65a0f78f
PecanPie <hidra0321@naver.com>
2023-06-12 18:18:16 +0900

    Added an old code that makes something Hashable

--- CodeTest_HashTables.playground/Pages/Example Code2.xcplaygroundpage/Contents.swift
+++ CodeTest_HashTables.playground/Pages/Example Code2.xcplaygroundpage/Contents.swift
@@ -1,7 +1,13 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+// MARK: PREVIOUSLY... WE USED TO CALCULATE HASHES in Swift
+struct GridPoint {
+    var x: Int
+    var y: Int
+    
+    var hashValue: Int {
+        /// 해시 값에 소수(Prime Number) 를 곱해서 완전히 겹치지 않는 수를 만든다.
+        return x.hashValue * y.hashValue * 16777619
+    }
+}
 
-//: [Next](@next)
