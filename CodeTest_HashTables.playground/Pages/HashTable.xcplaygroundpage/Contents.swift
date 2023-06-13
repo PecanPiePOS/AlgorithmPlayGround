eea028cfa54a2ef420db42d7bdd8c697363dfc06
PecanPie <hidra0321@naver.com>
2023-06-13 23:43:21 +0900

    Added HashTable Class and .put method

--- CodeTest_HashTables.playground/Pages/HashTable.xcplaygroundpage/Contents.swift
+++ CodeTest_HashTables.playground/Pages/HashTable.xcplaygroundpage/Contents.swift
@@ -20,10 +20,39 @@ class HashEntry {
     }
 }
 
-///// Hash Table
-//class HashTable {
-//    private static let initialSize = 256
-//    private var entries = [HashEntry?](repeating: nil, count: initialSize)
-//
-//
-//}
+/// Hash Table
+class HashTable {
+    private static let initialSize = 256
+    private var entries = [HashEntry?](repeating: nil, count: initialSize)
+
+    func put(key: String, value: String) {
+        let index = getIndex(key)
+        
+        let entry = HashEntry(key, value)
+        
+        if entries[index] == nil {
+            entries[index] = entry
+        } else {
+            var collisions = entries[index]
+            
+            while collisions?.next != nil {
+                collisions = collisions?.next
+            }
+            
+            collisions?.next = entry
+        }
+    }
+    
+    func getIndex(_ key: String) -> Int {
+        let hashCode = abs(key.hashValue)
+        
+        let index = hashCode % HashTable.initialSize
+        print("\(key) - \(hashCode) - \(index)")
+        
+        if key == "John Smith" || key == "Sandra Dee" {
+            return 152
+        }
+        
+        return index
+    }
+}
