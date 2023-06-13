a203a7f08bb004702de44d4e9dbd4d092e03b57d
PecanPie <hidra0321@naver.com>
2023-06-13 22:51:47 +0900

    Added Definition of Hash Table

--- CodeTest_HashTables.playground/Pages/HashTable.xcplaygroundpage/Contents.swift
+++ CodeTest_HashTables.playground/Pages/HashTable.xcplaygroundpage/Contents.swift
@@ -1,7 +1,8 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
-
-//: [Next](@next)
+/*
+ 
+ Generally, if the Hash Table Struct has been made correctly, the time Complexity of the functions will be O(1). Worst at O(N).
+ If Hash Collision happens it will be O(N)
+ 
+ */
