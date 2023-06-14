457bccc4d1d644235c53ecab5da14c966b7f0bbe
PecanPie <hidra0321@naver.com>
2023-06-14 23:25:01 +0900

    Added solution to leet13 which is almost same with my answer

--- CodeTest_HashTables.playground/Pages/LeetCode13_FasterSolution.xcplaygroundpage/Contents.swift
+++ CodeTest_HashTables.playground/Pages/LeetCode13_FasterSolution.xcplaygroundpage/Contents.swift
@@ -1,7 +1,16 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
-
-//: [Next](@next)
+class Solution2 {
+    private let dict: [Character:Int] = ["I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000]
+    
+    func romanToInt(_ s: String) -> Int {
+        var prev = 0, out = 0
+        for i in s {
+            let val = dict[i] ?? 0
+            out += val <= prev ? prev : -prev
+            prev = val
+        }
+        out += prev
+        return out
+    }
+}
