6f013f3d4eaddea16a1460e65cdb2b5d0d3106db
PecanPie <hidra0321@naver.com>
2023-06-14 23:26:09 +0900

    Added LeetCode128 Problem but not solved yet.

--- CodeTest_HashTables.playground/Pages/LeetCode128_MySolution.xcplaygroundpage/Contents.swift
+++ CodeTest_HashTables.playground/Pages/LeetCode128_MySolution.xcplaygroundpage/Contents.swift
@@ -1,7 +1,31 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+/*
+ 
+ Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
 
-//: [Next](@next)
+ You must write an algorithm that runs in O(n) time.
+ 
+ example)
+ Input: nums = [100,4,200,1,3,2]
+ Output: 4
+ Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+ 
+ */
+
+// https://leetcode.com/problems/longest-consecutive-sequence/
+
+class Solution {
+    func longestConsecutive(_ nums: [Int]) -> Int {
+        var numsSet = Set(nums)
+        var numsCount = numsSet.count
+        var previousCount = 0
+        var currentCount = 0
+        
+        for i in 0..<numsCount {
+            
+        }
+        
+        return 0
+    }
+}
