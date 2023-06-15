e8ed3f4fafd03dd0b5064030021224b44905a873
PecanPie <hidra0321@naver.com>
2023-06-15 18:42:27 +0900

    Added LeetCode 141 problem sheet for SLL. NOT solved yet

--- CodeTest_SinglyLinkedList.playground/Pages/LeetCode141_MySolution.xcplaygroundpage/Contents.swift
+++ CodeTest_SinglyLinkedList.playground/Pages/LeetCode141_MySolution.xcplaygroundpage/Contents.swift
@@ -1,7 +1,39 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+/*
+ 
+ Given head, the head of a linked list, determine if the linked list has a cycle in it.
 
-//: [Next](@next)
+ There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+ Return true if there is a cycle in the linked list. Otherwise, return false.
+ 
+ Input: head = [3,2,0,-4], pos = 1
+ Output: true
+ Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+ 
+ Input: head = [1,2], pos = 0
+ Output: true
+ Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+ 
+ Input: head = [1], pos = -1
+ Output: false
+ Explanation: There is no cycle in the linked list.
+ 
+ */
+
+    /// Definition for singly-linked list.
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
+class Solution {
+    func hasCycle(_ head: ListNode?) -> Bool {
+        
+    }
+}
