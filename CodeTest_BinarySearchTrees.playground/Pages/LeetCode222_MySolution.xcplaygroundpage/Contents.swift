a1c944158a1fa02fffc08f138f3efdf3d3f5c4a3
PecanPie <hidra0321@naver.com>
2023-06-19 19:26:52 +0900

    Not solved LeetCode 222

--- CodeTest_BinarySearchTrees.playground/Pages/LeetCode222_MySolution.xcplaygroundpage/Contents.swift
+++ CodeTest_BinarySearchTrees.playground/Pages/LeetCode222_MySolution.xcplaygroundpage/Contents.swift
@@ -1,7 +1,40 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+/*
+ 
+Given the root of a complete binary tree, return the number of the nodes in the tree.
 
-//: [Next](@next)
+According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Design an algorithm that runs in less than O(n) time complexity.
+ 
+ Input: root = [1,2,3,4,5,6]
+ Output: 6
+ 
+ Input: root = []
+ Output: 0
+ 
+ Input: root = [1]
+ Output: 1
+
+ */
+
+    /// Definition for a binary tree node.
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+class Solution {
+    func countNodes(_ root: TreeNode?) -> Int {
+        
+    }
+}
