0a1498fc020df094b919e776203a4c5522e00cbd
PecanPie <hidra0321@naver.com>
2023-06-19 19:24:46 +0900

    Added Traversing BST codes

--- CodeTest_BinarySearchTrees.playground/Pages/BinarySearchTree_Traverse_wt_Recursive.xcplaygroundpage/Contents.swift
+++ CodeTest_BinarySearchTrees.playground/Pages/BinarySearchTree_Traverse_wt_Recursive.xcplaygroundpage/Contents.swift
@@ -1,7 +1,89 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+class Node {
+    var key: Int = 0
+    var left: Node?
+    var right: Node?
+    
+    init(_ key: Int) {
+        self.key = key
+    }
+    
+    var min: Node {
+        if left == nil {
+            return self
+        } else {
+            return left!.min
+        }
+    }
+}
 
-//: [Next](@next)
+class BST {
+    
+    func printInOrderTravseral() {
+        inOrderTraversal(node: root)
+    }
+    
+    func inOrderTraversal(node: Node?) {
+        guard let node = node else { return }
+        inOrderTraversal(node: node.left)
+        print(node.key) // root
+        inOrderTraversal(node: node.right)
+    }
+    
+    func printPreOrderTravseral() {
+        preOrderTraversal(node: root)
+    }
+    
+    func preOrderTraversal(node: Node?) {
+        guard let node = node else { return }
+        print(node.key) // root
+        preOrderTraversal(node: node.left)
+        preOrderTraversal(node: node.right)
+    }
+    
+    func printPostOrderTravseral() {
+        postOrderTraversal(node: root)
+    }
+    
+    func postOrderTraversal(node: Node?) {
+        guard let node = node else { return }
+        postOrderTraversal(node: node.left)
+        postOrderTraversal(node: node.right)
+        print(node.key) // root
+    }
+    
+    func prettyPrint() {
+        // Hard code print for tree depth = 3
+        let rootLeftKey = root?.left == nil ? 0 : root?.left?.key
+        let rootRightKey = root?.right == nil ? 0 : root?.right?.key
+        
+        var rootLeftLeftKey = 0
+        var rootLeftRightKey = 0
+        
+        if root?.left != nil {
+            rootLeftLeftKey = root?.left?.left == nil ? 0 : root?.left?.left?.key as! Int
+            rootLeftRightKey = root?.left?.right == nil ? 0 : root?.left?.right?.key as! Int
+        }
+        
+        var rootRightLeftKey = 0
+        var rootRightRightKey = 0
+        
+        if root?.right != nil {
+            rootRightLeftKey = root?.right?.left == nil ? 0 : root?.right?.left?.key as! Int
+            rootRightRightKey = root?.right?.right == nil ? 0 : root?.right?.right?.key as! Int
+        }
+        
+        let str = """
+                          \(root!.key)
+                        /  \\
+                       \(rootLeftKey!)    \(rootRightKey!)
+                      / \\  / \\
+                     \(rootLeftLeftKey)  \(rootLeftRightKey) \(rootRightLeftKey)   \(rootRightRightKey)
+            """
+        
+        print(str)
+    }
+}
+
+
