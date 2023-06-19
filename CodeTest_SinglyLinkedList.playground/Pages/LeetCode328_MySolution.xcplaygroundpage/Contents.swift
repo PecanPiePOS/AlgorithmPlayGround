7533f51ec05485108140fda5eeb5a67eb5e77de2
PecanPie <hidra0321@naver.com>
2023-06-19 19:30:35 +0900

    Not getting anything out of it... Leet328

--- CodeTest_SinglyLinkedList.playground/Pages/LeetCode328_MySolution.xcplaygroundpage/Contents.swift
+++ CodeTest_SinglyLinkedList.playground/Pages/LeetCode328_MySolution.xcplaygroundpage/Contents.swift
@@ -1,7 +1,65 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+/*
+ 
+ Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
 
-//: [Next](@next)
+ The first node is considered odd, and the second node is even, and so on.
+
+ Note that the relative order inside both the even and odd groups should remain as it was in the input.
+
+ You must solve the problem in O(1) extra space complexity and O(n) time complexity.
+ 
+ 
+ Input: head = [1,2,3,4,5]
+ Output: [1,3,5,2,4]
+ 
+ Input: head = [2,1,3,5,6,4,7]
+ Output: [2,3,6,7,1,5,4]
+ 
+ */
+
+    /// Definition for singly-linked list.
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+ 
+    /// WHAT THE HELL IS WRONG WITH ANSWERS. I'M NOT GETTING ANYTHING.... 😰
+class Solution {
+    func oddEvenList(_ head: ListNode?) -> ListNode? {
+        var node = head
+        var evenHead = head
+        var oddHead = head
+        var oddCount = findOddCount(head)
+        
+        while node?.next != nil {
+            guard let data = node?.val else { return ListNode() }
+            if data % 2 == 1 {
+                
+            }
+            
+        }
+        
+        return head
+    }
+    
+    func findOddCount(_ head: ListNode?) -> Int {
+        var count = 0
+        var node = head
+        
+        while node?.next != nil {
+            guard let data = node?.val else { return 0 }
+            if data % 2 == 1 { // odd
+                count += 1
+            }
+            
+            node = node?.next
+        }
+        
+        return count
+    }
+}
--- CodeTest_SinglyLinkedList.playground/Pages/LeetCode328_Solution.xcplaygroundpage/Contents.swift
+++ CodeTest_SinglyLinkedList.playground/Pages/LeetCode328_Solution.xcplaygroundpage/Contents.swift
@@ -1,7 +1,32 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+/// Definition for singly-linked list.
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
 
-//: [Next](@next)
+class Solution {
+    func oddEvenList(_ head: ListNode?) -> ListNode? {
+        if head == nil {
+            return nil
+        }
+        
+        var odd: ListNode? = head
+        var even: ListNode? = head?.next
+        let evenHead: ListNode? = even
+        
+        while even != nil && even?.next != nil {
+            odd?.next = odd?.next?.next
+            even?.next = even?.next?.next
+            odd = odd?.next
+            even = even?.next
+        }
+        odd?.next = evenHead
+        
+        return head
+    }
+}
