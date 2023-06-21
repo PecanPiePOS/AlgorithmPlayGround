af99ba0a8be95b31b1f3d0c2a3fd9063a1895e97
PecanPie <hidra0321@naver.com>
2023-06-21 15:49:22 +0900

    Adding answers

--- CodeTest_BinarySearchTrees.playground/Pages/Example_Problem_1.xcplaygroundpage/Contents.swift
+++ CodeTest_BinarySearchTrees.playground/Pages/Example_Problem_1.xcplaygroundpage/Contents.swift
@@ -17,6 +17,40 @@ class Node {
 }
 
 func checkBST(root: Node?) -> Bool {
-    return isBSTValid(root, nil, nil)
+    return false
 }
+
+func isBSTValid(node: Node?) -> Node? {
+    guard let node,
+            let leftKey = node.left?.key,
+            let rightKey = node.right?.key else {
+        return nil
+    }
+    
+    var isValid: Bool = leftKey < node.key || rightKey > node.key
+    
+    switch isValid {
+    case true:
+        node = 
+        isBSTValid(node: node)
+    case false:
+        
+    }
+    
+    return nil
+}
+
+//func checkBST(root: Node?) -> Bool {
+//    return isBSTValid(root, nil, nil)
+//}
+//
+//func isBST(_ node: Node?, _ min: Int?, _ max: Int?) -> Bool {
+//    print("Comparing: \(node?.key) min: \(min) max: \(max)")
+//
+//
+//
+//
+//
+//    return false
+//}
 
