05e63ba723c990a3d4fb891b58fc16d0835e7fbd
PecanPie <hidra0321@naver.com>
2023-06-23 23:45:36 +0900

    Added solution to the example code but still not getting it whYgit add CodeTest_BinarySearchTrees.playground/Pages/Example_Problem_1.xcplaygroundpage/Contents.swiftgit add CodeTest_BinarySearchTrees.playground/Pages/Example_Problem_1.xcplaygroundpage/Contents.swift

--- CodeTest_BinarySearchTrees.playground/Pages/Example_Problem_1.xcplaygroundpage/Contents.swift
+++ CodeTest_BinarySearchTrees.playground/Pages/Example_Problem_1.xcplaygroundpage/Contents.swift
@@ -17,40 +17,48 @@ class Node {
 }
 
 func checkBST(root: Node?) -> Bool {
-    return false
+    return isBSTValid(root, nil, nil)
 }
 
-func isBSTValid(node: Node?) -> Node? {
-    guard let node,
-            let leftKey = node.left?.key,
-            let rightKey = node.right?.key else {
-        return nil
-    }
-    
-    var isValid: Bool = leftKey < node.key || rightKey > node.key
-    
-    switch isValid {
-    case true:
-        node = 
-        isBSTValid(node: node)
-    case false:
-        
-    }
-    
-    return nil
-}
+//func isBSTValid(node: Node?) -> Node? {
+//    guard let node,
+//            let leftKey = node.left?.key,
+//            let rightKey = node.right?.key else {
+//        return nil
+//    }
+//
+//    var isValid: Bool = leftKey < node.key || rightKey > node.key
+//
+//    switch isValid {
+//    case true:
+//        node =
+//        isBSTValid(node: node)
+//    case false:
+//
+//    }
+//
+//    return nil
+//}
 
-//func checkBST(root: Node?) -> Bool {
-//    return isBSTValid(root, nil, nil)
-//}
-//
-//func isBST(_ node: Node?, _ min: Int?, _ max: Int?) -> Bool {
-//    print("Comparing: \(node?.key) min: \(min) max: \(max)")
-//
-//
-//
-//
-//
-//    return false
-//}
+func isBST(_ node: Node?, _ min: Int?, _ max: Int?) -> Bool {
+    print("Comparing: \(node?.key) min: \(min) max: \(max)")
+    
+    // if nil we hit the end of the branch - OK
+    guard let node else { return true }
+    
+    // else check if min < parent
+    if let min, node.key <= min {
+        print("min: \(min) key: \(node.key)")
+        return false
+    }
+
+    // check if max > parent
+    if let max, node.key >= max {
+        print("max: \(max) key: \(node.key)")
+        return false
+    }
+
+    // if min max OK, go to next level passing in min/max and parent
+    return isBST(node.left, min, node.key)
+}
 
