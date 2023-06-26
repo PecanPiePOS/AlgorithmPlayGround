fab4ccdbfc0d8d8776010385a5252af2084b3413
PecanPie <hidra0321@naver.com>
2023-06-26 23:45:13 +0900

    Added LeetCode22 Solition

---
+++ CodeTest_DynamicProgramming.playground/Pages/LeetCode22_MySolution.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+/*
+ 
+ https://leetcode.com/problems/generate-parentheses/
+ 
+ Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ 
+ Input: n = 3
+ Output: ["((()))","(()())","(())()","()(())","()()()"]
+ 
+ Input: n = 1
+ Output: ["()"]
+ 
+ */
+
+class Solution {
+    func generateParenthesis(_ n: Int) -> [String] {
+        
+    }
+}
