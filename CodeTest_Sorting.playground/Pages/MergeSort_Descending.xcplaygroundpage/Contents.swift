fa02806ac9325925e38f09af804454f3a774fe09
PecanPie <hidra0321@naver.com>
2023-08-06 17:55:48 +0900

    Descending Merge Sorting Code Added

--- CodeTest_Sorting.playground/Pages/MergeSort_Descending.xcplaygroundpage/Contents.swift
+++ CodeTest_Sorting.playground/Pages/MergeSort_Descending.xcplaygroundpage/Contents.swift
@@ -1,7 +1,55 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
 
-//: [Next](@next)
+func mergeSortDescending<T: Comparable>(_ array: [T]) -> [T] {
+    let count = array.count
+    guard count > 1 else { return array }
+    
+    let midIndex = count/2
+    let left = Array(array[0..<midIndex])
+    let right = Array(array[midIndex..<count])
+    
+    return merge(left: mergeSortDescending(left), right: mergeSortDescending(right))
+}
+
+func merge<T: Comparable>(left: [T], right: [T]) -> [T] {
+    var leftArray = left
+    var rightArray = right
+    var result: [T] = []
+    
+    while !leftArray.isEmpty && !rightArray.isEmpty {
+        if leftArray[0] > rightArray[0] {
+            result.append(leftArray.removeFirst())
+        } else {
+            result.append(rightArray.removeFirst())
+        }
+    }
+    
+    if !leftArray.isEmpty {
+        result += leftArray
+    }
+    
+    if !rightArray.isEmpty {
+        result += rightArray
+    }
+    
+    return result
+}
+
+let array: [Int] = [9,47,34,72,1,23,66,54,43,78,70,71,99,6,555, 444, 444, 4256,24623,23424,234,24,52,523,62,62,4735,7,4573,5,62,4352,4,5234,624,372,57,2435,234,7,124,85,68,3,562,43,28,46,8568, 9,47,34,72,1,23,66,54,43,78,70,71,99,6,555, 444, 444, 4256,24623,23424,234,24,52,523,62,62,4735,7,4573,5,62,4352,4,5234,624,372,57,2435,234,7,124,85,68,3,562,43,28,46,8568,9,47,34,72,1,23,66,54,43,78,70,71,99,6,555, 444, 444, 4256,24623,23424,234,24,52,523,62,62,4735,7,4573,5,62,4352,4,5234,624,372,57,2435,234,7,124,85,68,3,562,43,28,46,8568]
+
+public func progressTime(_ closure: () -> ()) -> TimeInterval {
+    let start = CFAbsoluteTimeGetCurrent()
+    closure()
+    let diff = CFAbsoluteTimeGetCurrent() - start
+    print(diff)
+    
+    return (diff)
+}
+
+
+progressTime {
+    let k = mergeSortDescending(array)
+    print(k)
+}
+
